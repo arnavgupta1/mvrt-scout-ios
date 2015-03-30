09b9b74555c38046c6f024162c504de450563acb
Siddharth Gollapudi <siddharth@Siddharths-MacBook-Pro.local>
2015-03-30 16:53:57 -0700

    FINISHED UI!!!!

--- MVRTScouting/AutonController.swift
+++ MVRTScouting/AutonController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class AutonController: UITableViewController {
 
+    @IBOutlet weak var InterfereRobots: UISegmentedControl!
+    @IBOutlet weak var EndAutonZone: UISegmentedControl!
+    @IBOutlet weak var PickUpStgAreaCans: UISegmentedControl!
+    @IBOutlet weak var PickUpYellowTotes: UISegmentedControl!
+    @IBOutlet weak var StartLocation: UISegmentedControl!
+    @IBOutlet weak var GetCansFromStep: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
